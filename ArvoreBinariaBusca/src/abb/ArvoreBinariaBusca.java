86d0009b69884ed97b2996e6367514e8209525a6
Fabiana Pereira <fabianaduarte980@gmail.com>
2022-10-30 23:15:47 -0300

    adiciona função de pré ordem

--- ArvoreBinariaBusca/src/abb/ArvoreBinariaBusca.java
+++ ArvoreBinariaBusca/src/abb/ArvoreBinariaBusca.java
@@ -6,11 +6,49 @@ package abb;
  */
 public class ArvoreBinariaBusca {
 
+    public static String preOrdem(No raiz, String sequenciaAnterior) {
+        String sequencia = sequenciaAnterior + String.valueOf(raiz.getChave());
+
+        if (raiz.getEsquerda() != null) {
+            sequencia = preOrdem(raiz.getEsquerda(), sequencia);
+        }
+        if (raiz.getDireita() != null) {
+            sequencia = preOrdem(raiz.getDireita(), sequencia);
+        }
+
+        return sequencia;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        No um = new No(1);
+        No dois = new No(2);
+        No tres = new No(3);
+        No quatro = new No(4);
+        No cinco = new No(5);
+        No seis = new No(6);
+        No sete = new No(7);
+        No oito = new No(8);
+        No nove = new No(9);        
+
+        um.setEsquerda(dois);
+        um.setDireita(tres);
+
+        dois.setEsquerda(quatro);
+        dois.setDireita(sete);
+
+        tres.setEsquerda(cinco);
+        tres.setDireita(seis);
+
+        quatro.setDireita(nove);
+        
+        seis.setEsquerda(oito);
+
+        String sequencia = "";
+        sequencia = preOrdem(um, sequencia);
+        System.out.println(sequencia);
     }
-    
+
 }
